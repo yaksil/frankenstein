9d720612e6c4d4a019ccaaf548e55948f0931580
yaksil <zasvechenyi@gmail.com>
2021-04-30 19:30:16 +0300

    как выровнять это говно

--- client/src/layouts/components/Header.vue
+++ client/src/layouts/components/Header.vue
@@ -1,28 +1,58 @@
 <template>
   <header class="header">
     <div class="header-left">
-      <CvrseLogo />
+      <div class="cvrse">
+        cvrse
+      </div>
     </div>
     <div class="header-right">
+      <div class="navbar">
+        <ul>
+          <li>faq</li>
+          <li>search</li>
+        </ul>
+      </div>
     </div>
   </header>
 </template>
 
 <script>
-
 export default {
   name: "Header",
   components: {
-    CvrseLogo: () => import("@/layouts/components/CvrseLogo")
+
   }
 }
 </script>
 
 <style scoped>
 .header {
-  border-bottom: 1px #2c3e50;
-  padding: 10px;
+  padding-top: 5px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.cvrse {
+  font-family: Elephant, monospace;
+  font-size: 1.8em;
+  color: black;
+  padding-left: 20px;
+  padding-top: 10px;
+}
+
+.navbar {
   display: flex;
   justify-content: space-between;
+  align-items: baseline;
+  padding-right: 10px;
+}
+
+ul li {
+  font-family: "Yeseva One", monospace;
+  font-size: 1.2em;
+  list-style: none;
+  padding-right: 10px;
+  display: inline-flex;
 }
 </style>
