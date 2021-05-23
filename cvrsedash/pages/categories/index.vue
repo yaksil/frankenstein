79fb126773a0069cb490b07c88a4130b46866151
yaksil <zasvechenyi@gmail.com>
2021-05-23 14:09:57 +0300

    рефакторим рефакторим

---
+++ cvrsedash/pages/categories/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <cvrse-table />
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import CvrseTable from "@/components/cvrse-table";
+
+export default {
+  name: "categories-page",
+  components: {
+    CvrseTable,
+  },
+  mounted() {
+    this.fetchCategories();
+  },
+  computed: {
+    ...mapGetters({
+      categories: "categories/items"
+    })
+  },
+  methods: {
+    ...mapActions({
+      fetchCategories: "categories/fetchAll"
+    })
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ cvrsedash/pages/categories/setup.js
@@ -0,0 +1,15 @@
+export const columns = [
+  {
+    key: 'cat_name',
+    name: 'Название',
+  },
+]
+
+export const actions = [
+  {
+    className: '',
+    label: 'Редактировать',
+    emit: 'onEdit',
+    actionKey: '_id',
+  },
+]
